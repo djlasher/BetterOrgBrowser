c1771c00b49363d710e97383ffd3300166daceec
Clayton Blair <claytonoblair@live.com>
2026-06-07 07:26:34 -0700

    Expand manifest selection storage helper

--- src/packageXml/manifestSelectionStore.ts
+++ src/packageXml/manifestSelectionStore.ts
@@ -1,1 +1,15 @@
+import * as vscode from 'vscode';
+import { PackageXmlMember } from './packageXmlBuilder';
+
 export const manifestSelectionStorageKey = 'betterOrgBrowser.manifestSelections';
+
+export function loadManifestSelections(context: vscode.ExtensionContext): PackageXmlMember[] {
+    return context.workspaceState.get<PackageXmlMember[]>(manifestSelectionStorageKey, []);
+}
+
+export async function saveManifestSelections(
+    context: vscode.ExtensionContext,
+    selections: PackageXmlMember[]
+): Promise<void> {
+    await context.workspaceState.update(manifestSelectionStorageKey, selections);
+}
